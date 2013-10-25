f80f62aa4fe4b27e452d122f3caeee13a3093d86
Jozef Knaperek <jknaperek@gmail.com>
2013-10-25 19:29:23 +0200

    Add correct random functions

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
-# import random
-from numpy.random import poisson, exponential, uniform
+# from random import uniform
+from numpy.random import poisson, uniform
 
 # Car priorities for requesting slots in circles
 JUNCTION_PRIORITY_INSIDE = 1
@@ -7,6 +7,7 @@ JUNCTION_PRIORITY_JOINING = 2
 
 # Time it takes for moving by one car slot
 car_speed = 40  # km/h
+# car_speed = 80  # km/h
 slot_size = 3  # meters
 SLOT_PASSING_TIME = slot_size / (car_speed % 3.6)
 
@@ -32,27 +33,27 @@ CAR_GENERATOR_RANDOM_FUNCTIONS = [
 	# From exit 0 to exits 1, 2, 3
 	[
 		# West -> South
-		lambda: exponential(0.1),
+		lambda: poisson(20.72),
 		# West -> East
-		lambda: exponential(0.4),
+		lambda: poisson(8.42),
 		# West -> North
 		lambda: uniform(7, 83),
 	],
 	# From exit 1 to exits 2, 3, 0
 	[
 		# South -> East
-		lambda: poisson(0.9),
+		lambda: poisson(9.039),
 		# South -> North
-		lambda: exponential(0.65),
+		lambda: poisson(19.208),
 		# South -> West
-		lambda: poisson(4.3),
+		lambda: poisson(36.417),
 	],
 	# From exit 2 to exits 3, 0, 1
 	[
 		# East -> North
-		lambda: poisson(1.3),
+		lambda: poisson(9.125),
 		# East -> West
-		lambda: poisson(2.2),
+		lambda: poisson(9.319),
 		# East -> South
 		lambda: uniform(2, 82),
 	],
@@ -61,7 +62,7 @@ CAR_GENERATOR_RANDOM_FUNCTIONS = [
 		# North -> West
 		lambda: uniform(33, 100),
 		# North -> South
-		lambda: poisson(1.5),
+		lambda: poisson(24.11),
 		# North -> East
 		lambda: uniform(10, 71),
 	],
